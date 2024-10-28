107d1f433a71ac64cc7fb8fbb455cd5f056c091e
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-29 06:55:45 +0700

    Temukan karakter pertama yang tidak berulang dalam string dan return indexnya

---
+++ src/collectionproblemsolvingchallenge/FirstNonRepeatingCharacter.java
@@ -0,0 +1,41 @@
+package collectionproblemsolvingchallenge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String input1 = "leetcode";
+        String input2 = "loveleetcode";
+        String input3 = "aabb";
+        String input4 = "aabbaaccddeexxzz";
+        String input5 = "stress";
+
+        System.out.println(firstUniqChar(input1));
+        System.out.println(firstUniqChar(input2));
+        System.out.println(firstUniqChar(input3));
+        System.out.println(firstUniqChar(input4));
+        System.out.println(firstUniqChar(input5));
+    }
+
+    public static int firstUniqChar(String s) {
+        String[] str = s.split("");
+        Map<String, Integer> map = new HashMap<>();
+        int index = 0;
+        for(String string: str){
+            map.put(string, map.getOrDefault(string, 0) + 1);
+        }
+        System.out.println(map);
+        for(int i = 0; i < str.length; i++){
+            if(map.get(str[i]) == 1){
+                index = i;
+                break;
+            } else {
+                index = -1;
+            }
+        }
+        return index;
+    }
+}
