a23a9d0a198d19298bb75386c052cc556949917c
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-28 15:52:55 +0700

    Inisialisasi ArrayList menggunakan Arrays.asList()

--- src/ArrayListChallenge.java
+++ src/ArrayListChallenge.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListChallenge {
     public static void main(String[] args) {
         // 1. Buat sebuah empty ArrayList bertipe String
         ArrayList<String> list = new ArrayList<>();
         System.out.println(list);
+
+        // 2. Inisialisasi ArrayList menggunakan Arrays.asList()
+        String[] s = {"A","B","C"};
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList(s));
+        System.out.println(list1);
 
 
     }
