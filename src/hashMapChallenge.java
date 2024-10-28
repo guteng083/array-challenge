2cee34c0ad890f150ba59a983af85a191372bc97
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-28 18:50:31 +0700

    empty HashMap yang memetakan String ke Integer

--- src/hashMapChallenge.java
+++ src/hashMapChallenge.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class hashMapChallenge {
+    public static void main(String[] args) {
+        // 1. Buat empty HashMap yang memetakan String ke Integer
+        Map<String, String> map = new HashMap<>();
+        System.out.println(map);
+    }
 }
