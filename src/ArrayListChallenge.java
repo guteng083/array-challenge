df307f218847250db05aef876fddf8f30a014117
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-28 15:59:55 +0700

    [FIX]Fix typo

--- src/ArrayListChallenge.java
+++ src/ArrayListChallenge.java
@@ -5,17 +5,17 @@ import java.util.List;
 public class ArrayListChallenge {
     public static void main(String[] args) {
         // 1. Buat sebuah empty ArrayList bertipe String
-        ArrayList<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         System.out.println(list);
 
         // 2. Inisialisasi ArrayList menggunakan Arrays.asList()
         String[] s = {"A","B","C"};
-        ArrayList<String> list1 = new ArrayList<>(Arrays.asList(s));
+        List<String> list1 = new ArrayList<>(Arrays.asList(s));
         System.out.println(list1);
 
         // 3. Inisialisasi ArrayList menggunakan List.of()
         Integer[] i = {1,2,3};
-        ArrayList<Integer> list2 = new ArrayList<>(List.of(i));
+        List<Integer> list2 = new ArrayList<>(List.of(i));
         System.out.println(list2);
 
         // 4. Tambahkan single element ke ArrayList
