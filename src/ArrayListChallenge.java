28e5c7a901848d668eec3ac38ac56837b8f56396
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-28 15:40:52 +0700

    Make empty ArrayList with String datatype

---
+++ src/ArrayListChallenge.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayListChallenge {
+    public static void main(String[] args) {
+        // 1. Buat sebuah empty ArrayList bertipe String
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println(list);
+
+
+    }
+}
