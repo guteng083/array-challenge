77a97103e87e5d54d75bd643edd75f40d6cdbbde
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-10-28 18:52:02 +0700

    Put single entry ke HashMap

--- src/hashMapChallenge.java
+++ src/hashMapChallenge.java
@@ -4,7 +4,11 @@ import java.util.Map;
 public class hashMapChallenge {
     public static void main(String[] args) {
         // 1. Buat empty HashMap yang memetakan String ke Integer
-        Map<String, String> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        System.out.println(map);
+
+        // 2. Put single entry ke HashMap
+        map.put("satu", 1);
         System.out.println(map);
     }
 }
